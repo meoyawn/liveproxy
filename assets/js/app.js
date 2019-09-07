759f28707c4ce7bfdcc82fbd11a5d70a3439731f
adelnizamutdinov <stiggpwnz@gmail.com>
2019-09-07 20:55:55 +0300

    lighter

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,21 +3,7 @@
 // its own CSS file.
 import css from "../css/app.css"
 
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
 import "phoenix_html"
-
-// import "mdn-polyfills/NodeList.prototype.forEach"
-// import "mdn-polyfills/Element.prototype.closest"
-// import "mdn-polyfills/Element.prototype.matches"
-// import "child-replace-with-polyfill"
-// import "url-search-params-polyfill"
-// import "formdata-polyfill"
-// import "classlist-polyfill"
 import LiveSocket from "phoenix_live_view"
 
 let liveSocket = new LiveSocket("/live")
