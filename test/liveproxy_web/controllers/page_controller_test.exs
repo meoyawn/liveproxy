64039b6ada966b6a75e0d9d4cf3b382258a1331b
adelnizamutdinov <stiggpwnz@gmail.com>
2019-09-08 20:14:08 +0300

    test

--- test/liveproxy_web/controllers/page_controller_test.exs
+++ test/liveproxy_web/controllers/page_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule LiveproxyWeb.PageControllerTest do
-  use LiveproxyWeb.ConnCase
+  use LiveproxyWeb.ConnCase, async: true
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Proxy Checker"
   end
 end
