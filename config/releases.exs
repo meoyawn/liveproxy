0f740d0bf7393cab3b979b3ec57a24b08d2344d1
adelnizamutdinov <stiggpwnz@gmail.com>
2019-09-08 20:20:18 +0300

    specify url

--- config/releases.exs
+++ config/releases.exs
@@ -1,6 +1,7 @@
 import Config
 
 config :liveproxy, LiveproxyWeb.Endpoint,
+  url: [host: "104.248.142.159"],
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000"), compress: true]
 
 config :geolix,
