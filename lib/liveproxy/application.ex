fbc6399009bcc22c0ec2d47bd6638554c696fd26
adelnizamutdinov <stiggpwnz@gmail.com>
2019-09-13 14:18:27 +0300

    less conns

--- lib/liveproxy/application.ex
+++ lib/liveproxy/application.ex
@@ -6,7 +6,7 @@ defmodule Liveproxy.Application do
   use Application
 
   def start(_type, _args) do
-    :ok = :hackney_pool.start_pool(:massive, timeout: Proxy.timeout(), max_connections: 100_000)
+    :ok = :hackney_pool.start_pool(:massive, timeout: Proxy.timeout(), max_connections: 5_000)
 
     # List all child processes to be supervised
     children = [
